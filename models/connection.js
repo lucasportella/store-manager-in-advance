3a2a3fe96a2eccdfe42a08be052601d60d5f6720
lucas <lucasportellaagu@gmail.com>
2021-09-03 18:34:59 -0300

    create connection

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,29 @@
+const { MongoClient } = require('mongodb');
 
 // A conexão do banco local deverá conter os seguintes parâmetros:
 const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-const DB_NAME = 'StoreManager';
 
 // Para o avaliador funcionar altere a conexão do banco para:
 // const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-// const DB_NAME = 'StoreManager';
+
+const DB_NAME = 'StoreManager';
+
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let db = null;
+
+const connection = () =>
+  (
+    db ? 
+      Promise.resolve(db)
+      : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+        .then((conn) => {
+          db = conn.db('model_example');
+          return db;
+        }));
+
+module.exports = connection;

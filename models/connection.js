06ff86f141c49b81c1785bc739e55bd22213120e
lucas <lucasportellaagu@gmail.com>
2021-09-03 17:07:19 -0300

    create msc files

---
+++ models/connection.js
@@ -0,0 +1,8 @@
+
+// A conexão do banco local deverá conter os seguintes parâmetros:
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
+// Para o avaliador funcionar altere a conexão do banco para:
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const DB_NAME = 'StoreManager';

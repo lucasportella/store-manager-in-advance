87fa638072274d65224ae77ae4aa030d4cdcb0f0
lucas <lucasportellaagu@gmail.com>
2021-09-04 12:53:15 -0300

    return created status

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,10 +1,10 @@
 const productsService = require('../services/productsService');
-const {  ok, } = require('../utils/statusCodes');
+const { created } = require('../utils/statusCodes');
 
 const createProduct = async (req, res) => {
   const { name, quantity } = req.body;
   const result = await productsService.createProduct(name, quantity);
-  return res.status(ok).json(result);
+  return res.status(created).json(result);
 };
 
 module.exports = {

8db1f33f1b768e27c79a30a7ce87e1e905149320
lucas <lucasportellaagu@gmail.com>
2021-09-08 13:26:51 -0300

    model test start

---
+++ tests/unit/models.test.js
@@ -0,0 +1,43 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const productsModel = require('../../models/productsModel');
+
+describe('test model layer', () => {
+  before( async () => {
+    const DBserver = new MongoMemoryServer();
+    const URLMock = await DBserver.getUri();
+
+    const connectionMock = await MongoClient.connect(URLMock, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+
+    after(() => {
+      MongoClient.connect.restore();
+    });
+
+  });
+  
+  describe('Create new Product', () => {
+    const payloadProduct = {
+      'name': 'tomatoes',
+      'quantity': 200
+    };
+  
+    describe('When creation is successful', () => {
+  
+      it('have _id, name, quantity properties', async () => {
+        const response = await productsModel
+          .createProduct(payloadProduct.name, payloadProduct.quantity);
+        expect(response).to.have.all.keys('_id', 'name', 'quantity');
+      });
+  
+    });
+  
+  });
+});
